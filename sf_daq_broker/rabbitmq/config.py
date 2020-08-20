059c9101fae2d930edaa001793b54ae91003f6ec
Andrej Babic <andrej.babic@psi.ch>
2020-08-20 09:51:54 +0200

    Add basic config to broker

---
+++ sf_daq_broker/rabbitmq/config.py
@@ -0,0 +1,7 @@
+DEFAULT_BROKER_URL = "127.0.0.1"
+
+# Exchange where the write requests are sent.
+REQUEST_EXCHANGE = "request"
+
+# Exchange where the status of the writing is reported.
+STATUS_EXCHANGE = "status"
