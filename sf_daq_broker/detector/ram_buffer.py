8f4ca8a781ed9457c6a4295e7975154709ecd893
Andrej Babic <andrej.babic@psi.ch>
2020-11-16 17:49:27 +0100

    Finished image meta retrieval mechanism

--- sf_daq_broker/detector/ram_buffer.py
+++ sf_daq_broker/detector/ram_buffer.py
@@ -11,7 +11,7 @@ class RamBuffer(object):
 
         self.raw_buffer = bytearray(self.n_modules * self.n_slots * BUFFER_FRAME_BYTES)
 
-    def get_buffers(self, module_id, pulse_id):
+    def get_frame_buffers(self, module_id, pulse_id):
         # pulse_id index in RAM buffer
         buffer_index = pulse_id % self.n_slots
         # pulse_id index bytes offset in RAM buffer
@@ -23,6 +23,25 @@ class RamBuffer(object):
 
         # Pulse_id slot offset + metadata buffer offset + module_id offset in data buffer.
         data_offset_start = pulse_id_offset + (self.n_modules * META_FRAME_BYTES) + (module_id * DATA_FRAME_BYTES)
+        data_offset_end = data_offset_start + DATA_FRAME_BYTES
+
+        meta_buffer = memoryview(self.raw_buffer)[meta_offset_start:meta_offset_end]
+        data_buffer = memoryview(self.raw_buffer)[data_offset_start:data_offset_end]
+
+        return meta_buffer, data_buffer
+
+    def get_image_buffers(self, pulse_id):
+        # pulse_id index in RAM buffer
+        buffer_index = pulse_id % self.n_slots
+        # pulse_id index bytes offset in RAM buffer
+        pulse_id_offset = buffer_index * self.n_modules * BUFFER_FRAME_BYTES
+
+        # Pulse_id slot.
+        meta_offset_start = pulse_id_offset
+        meta_offset_end = meta_offset_start + META_FRAME_BYTES * self.n_modules
+
+        # Pulse_id slot offset + data buffer offset
+        data_offset_start = pulse_id_offset + META_FRAME_BYTES * self.n_modules
         data_offset_end = data_offset_start + DATA_FRAME_BYTES
 
         meta_buffer = memoryview(self.raw_buffer)[meta_offset_start:meta_offset_end]
