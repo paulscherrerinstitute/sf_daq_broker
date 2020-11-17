4ca17618769cf8aa2cc35bfbc192c93325b189e2
Andrej Babic <andrej.babic@psi.ch>
2020-11-17 11:42:34 +0100

    Finalize ImageAssembler test

--- tests/test_image_assembler.py
+++ tests/test_image_assembler.py
@@ -1,7 +1,6 @@
 import struct
 import unittest
 
-import numpy
 import zmq
 
 from sf_daq_broker.detector.buffer_reader import get_push_sender
@@ -14,11 +13,29 @@ class TestImageAssembler(unittest.TestCase):
     def test_for_errors(self):
 
         context = zmq.Context()
+
+        with self.assertRaisesRegex(ValueError, "The buffer must have at least 5 slots."):
+            ImageAssembler(RamBuffer(n_modules=1, n_slots=4), n_modules=3, zmq_context=context)
+
         ram_buffer = RamBuffer(n_modules=3, n_slots=5)
         image_assembler = ImageAssembler(ram_buffer=ram_buffer, n_modules=3, zmq_context=context)
+        senders = []
 
         with self.assertRaisesRegex(TimeoutError, "No data from buffer readers."):
             image_assembler.get_image(100)
+
+        for i_module in range(3):
+            sender = get_push_sender(context)
+            sender.bind("inproc://%d" % i_module)
+            senders.append(sender)
+
+            sender.send(struct.pack("Q", 101))
+
+        with self.assertRaisesRegex(ValueError, "Synchronization error"):
+            image_assembler.get_image(102)
+
+        # TODO: Test also image reconstruction missmatch
+        #  (when modules are put together, pulse_id in metadata must match)
 
     def test_simple_assembly(self):
         n_modules = 3
