e366bd467e40de984981f631a2245271b5f27e5f
Data Backend account <dbe@sf-daq-1.psi.ch>
2020-12-09 11:00:58 +0100

    Include raw class and modify output file

--- tests/perf_detector_raw_writer.py
+++ tests/perf_detector_raw_writer.py
@@ -1,8 +1,61 @@
 import numpy
+import h5py
+import numpy
+import zmq
+class DetectorRawWriter(object):
+    def __init__(self, output_file, n_images, n_modules, metadata):
+        self.output_file = output_file
+        self.n_modules = n_modules
+        self.metadata = metadata
+        # TODO: Put real name here, please.
+        self.detector_name = "detector"
 
-from sf_daq_broker.writer.detector_raw_writer import DetectorRawWriter
+        self.pulse_id_cache = numpy.zeros(shape=n_images, dtype="uint64")
+        self.frame_index_cache = numpy.zeros(shape=n_images, dtype="uint64")
+        self.daq_rec_cache = numpy.zeros(shape=n_images, dtype="uint32")
+        self.is_good_frame_cache = numpy.zeros(shape=n_images, dtype="uint8")
 
-output_file = "/sf/alvra/data/p17502/raw/test_python.h5"
+        self.file = h5py.File(self.output_file, "w")
+
+        self._create_metadata_datasets(metadata)
+
+        self.current_write_index = 0
+
+        self._image_dataset = self.file.create_dataset(
+            name="/data/" + self.detector_name + "/data",
+            dtype="uint16",
+            shape=(n_images, self.n_modules * 512, 1024),
+            chunks=(1, self.n_modules * 512, 1024)
+        )
+
+    def _create_metadata_datasets(self, metadata):
+
+        for key, value in metadata.items():
+            self.file.create_dataset(key, data=numpy.string_(value))
+
+    def write(self, pulse_id, meta_buffer, data_buffer):
+        self._image_dataset.id.write_direct_chunk((self.current_write_index, 0, 0),
+                                                  data_buffer[:])
+
+        self.pulse_id_cache[self.current_write_index] = meta_buffer["pulse_id"]
+        self.frame_index_cache[self.current_write_index] = meta_buffer["frame_index"]
+        self.daq_rec_cache[self.current_write_index] = meta_buffer["daq_rec"]
+        self.is_good_frame_cache[self.current_write_index] = meta_buffer["is_good_image"]
+
+        self.current_write_index += 1
+
+    def _write_metadata(self):
+        self.file["/data/" + self.detector_name + "/pulse_id"] = self.pulse_id_cache
+        self.file["/data/" + self.detector_name + "/frame_index"] = self.frame_index_cache
+        self.file["/data/" + self.detector_name + "/daq_rec"] = self.daq_rec_cache
+        self.file["/data/" + self.detector_name + "/is_good_frame"] = self.is_good_frame_cache
+
+    def close(self):
+        self._write_metadata()
+        self.file.close()
+
+
+output_file = "/sf/alvra/data/p17502/raw/test_python_direct.h5"
 n_images = 10000
 n_modules = 4
 metadata = {}
