a4b55422351cb6f7311e82e9de0c0c172ec289f2
Dmitry Ozerov <dmitry.ozerov@psi.ch>
2021-01-18 17:48:21 +0100

    move all detector specific constants (not daq computer related) to inventory

--- operation-tools/templates/streamvis.sh
+++ operation-tools/templates/streamvis.sh
@@ -6,7 +6,7 @@ conda deactivate
 conda activate vis
 
 PORT={{ visualisation_port }}
-PORT_BACKEND={{ incoming_data_port }}
+PORT_BACKEND={{ visualisation_incoming_data_port }}
 
 H=`echo ${HOSTNAME} | sed 's/.psi.ch//'`
 BACKEND=${H}
@@ -14,7 +14,7 @@ BACKEND=${H}
 CORES="{{ visualisation_cores }}"
 
 taskset -c ${CORES} \
-streamvis {{ visualisation_view }} --allow-websocket-origin=${H}:${PORT} --allow-websocket-origin={{ alias_visualisation }}:${PORT} \
+streamvis {{ visualisation_view }} --allow-websocket-origin=${H}:${PORT} --allow-websocket-origin={{ visualisation_alias }}:${PORT} \
 --port=${PORT} --address  tcp://${BACKEND}:${PORT_BACKEND} \
 --page-title {{ visualisation_title }}
 
