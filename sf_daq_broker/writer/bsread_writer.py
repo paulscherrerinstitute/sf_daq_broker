b8056e640c260ac77378b852d1318f15196178b1
Dmitry Ozerov <dmitry.ozerov@psi.ch>
2021-01-26 18:07:26 +0100

    check consistency of retrieved data from data buffer by retrieve version 2 method

--- sf_daq_broker/writer/bsread_writer.py
+++ sf_daq_broker/writer/bsread_writer.py
@@ -101,6 +101,10 @@ def write_from_databuffer_api3(data_api_request, output_file, parameters):
 
     _logger.debug("Data3 API request: %s", data_api_request)
 
+    start_pulse_id = data_api_request["range"]["startPulseId"]
+    stop_pulse_id  = data_api_request["range"]["endPulseId"]
+    rate_multiplicator = data_api_request.get("rate_multiplicator", 1)
+
     data_api_request_timestamp = utils.transform_range_from_pulse_id_to_timestamp(data_api_request)
 
     channels = [channel["name"] for channel in data_api_request_timestamp["channels"]]
@@ -132,6 +136,60 @@ def write_from_databuffer_api3(data_api_request, output_file, parameters):
     except Exception as e:
         _logger.error("Got exception from data_api3")
         _logger.error(e)
+
+    start_check_time = time()
+
+    # run checks
+    expected_pulse_id = []
+    for p in range(start_pulse_id,stop_pulse_id+1):
+        if p%rate_multiplicator == 0:
+            expected_pulse_id.append(p)
+    expected_pulse_id = numpy.array(expected_pulse_id)
+    expected_number_measurements = len(expected_pulse_id)
+
+    try:
+        bsread_data_api3_h5py = h5py.File(output_file,"r")
+        inside_data_api3_file = list(bsread_data_api3_h5py.keys())
+        for channel in channels:
+            if channel not in inside_data_api3_file:
+                _logger.error(f'check channel {channel} not present in data_api3 file')
+            else:
+
+                pulse_id_raw    = bsread_data_api3_h5py[f'/{channel}/pulse_id'][:]
+
+                if numpy.sum(pulse_id_raw < start_pulse_id) or numpy.sum(pulse_id_raw > stop_pulse_id):
+                    _logger.error(f'check channel {channel} contains pulse_id outside of requested range')
+
+                n_pulse_id_raw = len(pulse_id_raw)
+
+                n_unique_pulse_id_raw = len(set(pulse_id_raw))
+
+                duplicated_entries = False
+                if n_pulse_id_raw != n_unique_pulse_id_raw:
+                    duplicated_entries = True
+                    _logger.error(f'check {channel} contains duplicated entries. Total entries : {n_pulse_id_raw}, duplicated entries : {n_pulse_id_raw-n_unique_pulse_id_raw} ')
+
+                pulse_id = numpy.intersect1d(expected_pulse_id, pulse_id_raw)
+                n_pulse_id = len(pulse_id) 
+
+                if n_pulse_id != expected_number_measurements:
+                    _logger.error(f'check {channel} number of pulse_id(unique) is different from expected : {n_pulse_id} vs {expected_number_measurements}')
+                else:
+                    if pulse_id[0] != expected_pulse_id[0] or pulse_id[-1] != expected_pulse_id[-1]:
+                        _logger.error(f'check {channel} start/stop pulse_id are not the one which are requested (requested : {expected_pulse_id[0]},{expected_pulse_id[-1]}, got: {pulse_id[0]},{pulse_id[-1]}) ')
+                        if not duplicated_entries:
+                            pulse_id_check = True 
+                            for i in range(n_pulse_id):
+                                if pulse_id[i] != expected_pulse_id[i]:
+                                    pulse_id_check = False
+                            if not pulse_id_check:
+                                _logger.error(f'check {channel} pulse_id are not monotonic')
+
+    except Exception as e:
+        _logger.error(f'check failed')
+        _logger.error(e)
+
+    _logger.info("Check of data consistency took %s seconds." % (time() - start_check_time))
 
 
 class BsreadH5Writer(object):
