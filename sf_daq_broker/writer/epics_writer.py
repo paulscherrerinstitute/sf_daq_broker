2f87da5ecfe5a1f0255b4a1d201c772127f9c3c7
Andrej Babic <andrej.babic@psi.ch>
2021-02-01 11:13:51 +0100

    Change epics_writer mapping to new data api mapping

--- sf_daq_broker/writer/epics_writer.py
+++ sf_daq_broker/writer/epics_writer.py
@@ -4,18 +4,15 @@ from time import time, sleep
 
 import dateutil.parser
 import h5py
-import pytz
 import requests
 import numpy
 
+from sf_daq_broker.utils import pulse_id_to_seconds
 from sf_daq_broker.writer.bsread_writer import BsreadH5Writer
 
 _logger = logging.getLogger("broker_writer")
 
 DATA_API_QUERY_URL = "https://data-api.psi.ch/sf/query"
-PULSE_ID_MAPPING_CHANNEL = "SIN-CVME-TIFGUN-EVR0:BUNCH-1-OK"
-TIMEZONE = pytz.timezone('Europe/Zurich')
-TARGET_MAPPING_DELAY_SECONDS = 30
 N_RETRY_LIMIT = 5
 N_RETRY_TIMEOUT = 10
 
@@ -27,10 +24,10 @@ def write_epics_pvs(output_file, start_pulse_id, stop_pulse_id, metadata, epics_
 
     start_time = time()
 
-    start_date = get_pulse_id_date_mapping(start_pulse_id)
-    stop_date = get_pulse_id_date_mapping(stop_pulse_id)
+    start_seconds = pulse_id_to_seconds(start_pulse_id)
+    stop_seconds = pulse_id_to_seconds(stop_pulse_id)
 
-    data = get_data(epics_pvs, start=start_date, stop=stop_date)
+    data = get_data(epics_pvs, start_seconds=start_seconds, stop_seconds=stop_seconds)
 
     _logger.info("Data download took %s seconds." % (time() - start_time))
 
@@ -40,17 +37,17 @@ def write_epics_pvs(output_file, start_pulse_id, stop_pulse_id, metadata, epics_
     start_time = time()
 
     with EpicsH5Writer(output_file, metadata) as writer:
-        writer.write_data(data, start_date)
+        writer.write_data(data, start_seconds)
 
     _logger.info("Data writing took %s seconds." % (time() - start_time))
 
 
-def get_data(channel_list, start=None, stop=None):
+def get_data(channel_list, start_seconds=None, stop_seconds=None):
     _logger.info("Requesting range %s to %s for channels: %s" %
-                 (start, stop, channel_list))
+                 (start_seconds, stop_seconds, channel_list))
 
-    query = {"range": {"startDate": datetime.isoformat(start),
-                       "endDate": datetime.isoformat(stop),
+    query = {"range": {"startSeconds": start_seconds,
+                       "endSeconds": stop_seconds,
                        "startExpansion": True,
                        "endInclusive": True},
              "channels": channel_list,
@@ -72,59 +69,6 @@ def get_data(channel_list, start=None, stop=None):
         return response.json()
 
     raise RuntimeError("Unable to retrieve data from server: ", response)
-
-
-def get_pulse_id_date_mapping(pulse_id):
-    _logger.info("Retrieve pulse-id/date mapping for pulse_id %s" % pulse_id)
-
-    try:
-        query = {"range": {"startPulseId": 0,
-                           "endPulseId": pulse_id},
-                 "limit": 1,
-                 "ordering": "desc",
-                 "channels": [PULSE_ID_MAPPING_CHANNEL],
-                 "fields": ["pulseId", "globalDate"]}
-
-        def retrieve_mapping():
-
-            response = requests.post("https://data-api.psi.ch/sf/query", json=query)
-
-            # Check for successful return of data
-            if response.status_code != 200:
-                raise RuntimeError("Unable to retrieve data from data-api: ", response)
-
-            try:
-                data = response.json()
-                mapping_data = data[0]["data"][0]
-                mapping_pulse_id = mapping_data["pulseId"]
-                mapping_global_date = mapping_data["globalDate"]
-
-            except Exception as ex:
-                raise RuntimeError("Unexpected response from data-api: %s" % data) from ex
-
-            return mapping_pulse_id, dateutil.parser.parse(mapping_global_date)
-
-        received_pulse_id, global_date = retrieve_mapping()
-
-        if received_pulse_id != pulse_id:
-            _logger.warning("Requested pulse_id %s but received %s.")
-
-            time_since_last_record = (datetime.now(TIMEZONE) - global_date).total_seconds()
-            to_sleep = max(0., TARGET_MAPPING_DELAY_SECONDS - time_since_last_record)
-
-            if to_sleep:
-                _logger.info("Retrying in %s seconds." % to_sleep)
-                sleep(to_sleep)
-
-                received_pulse_id, global_date = retrieve_mapping()
-
-            if received_pulse_id != pulse_id:
-                raise RuntimeError('Cannot find requested pulse_id.')
-
-        return global_date
-
-    except Exception as e:
-        raise RuntimeError('Cannot map pulse_id to global_date.') from e
 
 
 class EpicsH5Writer(BsreadH5Writer):
