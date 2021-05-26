e7260a128a7020f9cc07db841baab47fea94d419
Dmitry Ozerov <dmitry.ozerov@psi.ch>
2021-05-26 16:13:02 +0200

    different exposure time for Maloja detector

--- detector/start_detector.sh
+++ detector/start_detector.sh
@@ -40,6 +40,15 @@ case ${DETECTOR} in
 'JF10')
   D=10
   ;;
+'JF10')
+  D=10
+  ;;
+'JF14')
+  D=14
+  ;;
+'JF15')
+  D=15
+  ;;
 *)
   echo "Unsupported detector"
   exit
@@ -59,7 +68,12 @@ conda activate sf-daq
 sls_detector_put ${D}-stop
 sls_detector_put ${D}-timing trigger
 sls_detector_put ${D}-triggers ${n_cycles}
-sls_detector_put ${D}-exptime 5us
+if [ ${DETECTOR} == "JF15" ]
+then
+  sls_detector_put ${D}-exptime 10us
+else 
+  sls_detector_put ${D}-exptime 5us
+fi
 sls_detector_put ${D}-frames 1
 sls_detector_put ${D}-dr 16
 #sls_detector_put ${D}-clearbit to 0x5d 0 # normal mode, not highG0
