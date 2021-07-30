643bd3a3618e13e5d120735bf4fca93f78325905
Dmitry Ozerov <dmitry.ozerov@psi.ch>
2021-07-30 16:29:05 +0200

    double pixels treatment

--- sf_daq_broker/writer/export_file.py
+++ sf_daq_broker/writer/export_file.py
@@ -20,32 +20,28 @@ def convert_file(file_in, file_out, json_run_file, detector_config_file):
         data = json.load(detector_file)
 
         detector_name = data["detector_name"]
-        gain_file = data["gain_file"]
+        gain_file     = data["gain_file"]
         pedestal_file = data["pedestal_file"]
 
     with open(json_run_file, "r") as run_file:
         data = json.load(run_file)
         detector_params = data["detectors"][detector_name]
 
-        compression = detector_params.get("compression", False)
-        conversion = detector_params.get("adc_to_energy", False)
+        compression      = detector_params.get("compression", False)
+        conversion       = detector_params.get("adc_to_energy", False)
         disabled_modules = detector_params.get("disabled_modules", [])
         if conversion:
-            mask = detector_params.get("mask", True)
-            mask_double_pixels = detector_params.get("mask_double_pixels", True)
-            geometry = detector_params.get("geometry", False)
-            gap_pixels = detector_params.get("gap_pixels", True)
-            factor = detector_params.get("factor", None)
+            mask                 = detector_params.get("mask", True)
+            double_pixels_action = detector_params.get("double_pixels_action", "mask")
+            geometry             = detector_params.get("geometry", False)
+            gap_pixels           = detector_params.get("gap_pixels", True)
+            factor               = detector_params.get("factor", None)
         else:
             mask = False
-            mask_double_pixels = False
+            double_pixels_action = "keep"
             geometry = False
             gap_pixels = False
             factor = None
-
-    if not mask and mask_double_pixels:
-        _logger.error("mask_double_pixels set to False")
-        mask_double_pixels = False
 
     file_tmp = file_in
     if len(disabled_modules)>0:
@@ -64,6 +60,7 @@ def convert_file(file_in, file_out, json_run_file, detector_config_file):
             pedestal_file=pedestal_file,
             conversion=conversion,
             mask=mask,
+            double_pixels=double_pixels_action,
             gap_pixels=gap_pixels,
             geometry=geometry,
             parallel=False,
@@ -72,7 +69,6 @@ def convert_file(file_in, file_out, json_run_file, detector_config_file):
             good_frames = np.nonzero(juf["is_good_frame"])[0]
             n_output_frames = len(good_frames)
 
-            juf.handler.mask_double_pixels = mask_double_pixels
             juf.export(
                 file_out,
                 index=good_frames,
@@ -92,21 +88,21 @@ def convert_file(file_in, file_out, json_run_file, detector_config_file):
 
     # Utility info
     with h5py.File(file_out, "r") as h5f:
-        _logger.info("daq_rec: %s" % h5f[f"/data/{detector_name}/daq_rec"][0, 0])
+        _logger.info("daq_rec          : %s" % h5f[f"/data/{detector_name}/daq_rec"][0, 0])
 
         frame_index = h5f[f"/data/{detector_name}/frame_index"][:]
         _logger.info("frame_index range: (%d - %d)" % (np.min(frame_index), np.max(frame_index)))
 
-    _logger.info(f"input frames: {n_input_frames}")
-    _logger.info(f"bad frames: {n_input_frames - n_output_frames}")
+    _logger.info(f"input frames : {n_input_frames}")
+    _logger.info(f"bad frames   : {n_input_frames - n_output_frames}")
     _logger.info(f"output frames: {n_output_frames}")
 
-    _logger.info(f"gain_file: {gain_file}")
-    _logger.info(f"pedestal_file: {pedestal_file}")
-    _logger.info(f"conversion: {conversion}")
-    _logger.info(f"mask: {mask}")
-    _logger.info(f"mask_double_pixels:x {mask_double_pixels}")
-    _logger.info(f"geometry: {geometry}")
-    _logger.info(f"gap_pixels: {gap_pixels}")
-    _logger.info(f"compression: {compression}")
-    _logger.info(f"factor: {factor}")
+    _logger.info(f"gain_file           : {gain_file}")
+    _logger.info(f"pedestal_file       : {pedestal_file}")
+    _logger.info(f"conversion          : {conversion}")
+    _logger.info(f"mask                : {mask}")
+    _logger.info(f"double_pixels_action: {double_pixels_action}")
+    _logger.info(f"geometry            : {geometry}")
+    _logger.info(f"gap_pixels          : {gap_pixels}")
+    _logger.info(f"compression         : {compression}")
+    _logger.info(f"factor              : {factor}")
