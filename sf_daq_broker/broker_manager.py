b605ec3e81610ed82c71c73f63089ccffc469a8e
Dmitry Ozerov <dmitry.ozerov@psi.ch>
2022-05-07 23:20:04 +0200

    configuration of Cristallina-MX detector

--- sf_daq_broker/broker_manager.py
+++ sf_daq_broker/broker_manager.py
@@ -23,7 +23,7 @@ _logger = logging.getLogger(__name__)
 
 allowed_detectors_beamline = { "alvra"       : [ "JF02T09V03", "JF06T32V02"],
                                "bernina"     : [ "JF01T03V01", "JF03T01V02", "JF04T01V01", "JF05T01V01", "JF13T01V01", "JF14T01V01"],
-                               "cristallina" : [ "JF16T03V01"],
+                               "cristallina" : [ "JF16T03V01", "JF17T16V01"],
                                "furka"       : [],
                                "maloja"      : [ "JF15T08V01"]
                              }
