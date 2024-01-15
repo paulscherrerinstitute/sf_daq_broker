a0973b58fd7baebb9335c90d44f9759a90a233da
NichtJens <nichtjens@gmail.com>
2024-01-16 00:02:41 +0100

    use with statement; removed useless parenthesis

--- client/client_example.py
+++ client/client_example.py
@@ -80,9 +80,8 @@ class BrokerClient:
             beamline=get_beamline()
             last_run_file = f"/sf/{beamline}/data/{self.pgroup}/raw/run_info/LAST_RUN"
             if os.path.exists(last_run_file):
-                run_file = open(last_run_file, "r")
-                self.last_run = int(run_file.read())
-                run_file.close()
+                with open(last_run_file, "r") as run_file:
+                    self.last_run = int(run_file.read())
         except:
             pass
 
@@ -133,7 +132,7 @@ class BrokerClient:
                 if current_pulseid >= stop_pulseid:
                     break
                 time.sleep(0.1)
-                progress = ((current_pulseid-self.start_pulseid)/(stop_pulseid-self.start_pulseid))
+                progress = (current_pulseid-self.start_pulseid) / (stop_pulseid-self.start_pulseid)
                 block = int(round(20*progress))
                 text = "\r[{0}] {1}% Run: {2}".format( "#"*block + "-"*(20-block), int(progress*100), last_known_run+1)
                 sys.stdout.write(text)
