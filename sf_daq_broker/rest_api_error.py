67beff2ba23346318657982b4c4d86ba207eb79d
Sven Augustin <sven.augustin@psi.ch>
2024-01-22 12:18:34 +0100

    separated/re-use the error handler

---
+++ sf_daq_broker/rest_api_error.py
@@ -0,0 +1,24 @@
+import json
+import logging
+
+import bottle
+
+
+_logger = logging.getLogger(__name__)
+
+
+def register_error_handler(app):
+
+    @app.error(500)
+    def error_handler_500(error):
+        bottle.response.content_type = "application/json"
+        bottle.response.status = 200
+
+        error_text = str(error.exception)
+
+        _logger.error(error_text)
+
+        return json.dumps({
+            "state": "error",
+            "status": error_text
+        })
--- sf_daq_broker/rest_api_slow.py
+++ sf_daq_broker/rest_api_slow.py
@@ -1,12 +1,11 @@
-import json
-import logging
-
 import bottle
 
-_logger = logging.getLogger(__name__)
+from .rest_api_error import register_error_handler
 
 
 def register_rest_interface(app, manager):
+
+    register_error_handler(app)
 
     @app.post("/get_detector_settings")
     def get_detector_settings():
@@ -27,15 +26,3 @@ def register_rest_interface(app, manager):
     @app.post("/set_dap_settings")
     def set_dap_settings():
         return manager.set_dap_settings(request=bottle.request.json, remote_ip=bottle.request.remote_addr)
-
-    @app.error(500)
-    def error_handler_500(error):
-        bottle.response.content_type = "application/json"
-        bottle.response.status = 200
-
-        error_text = str(error.exception)
-
-        _logger.error(error_text)
-
-        return json.dumps({"state": "error",
-                           "status": error_text})
