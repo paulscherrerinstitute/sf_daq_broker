6a9b6cefa1e23866e5adc5be39fcd3446aaebf31
Sven Augustin <sven.augustin@psi.ch>
2024-01-24 11:49:50 +0100

    readability

--- sf_daq_broker/writer/utils.py
+++ sf_daq_broker/writer/utils.py
@@ -24,17 +24,20 @@ def deserialize_string(numpy_array):
 
 channel_type_deserializer_mapping = {
     # Default value if no channel_type specified.
-    None: ("f8", deserialize_number),
-    "int8": ("i1", deserialize_number),
-    "uint8": ("u1", deserialize_number),
-    "int16": ("i2", deserialize_number),
-    "uint16": ("u2", deserialize_number),
-    "int32": ("i4", deserialize_number),
-    "uint32": ("u4", deserialize_number),
-    "int64": ("i8", deserialize_number),
-    "uint64": ("u8", deserialize_number),
+    None:      ("f8", deserialize_number),
+    "int8":    ("i1", deserialize_number),
+    "uint8":   ("u1", deserialize_number),
+    "int16":   ("i2", deserialize_number),
+    "uint16":  ("u2", deserialize_number),
+    "int32":   ("i4", deserialize_number),
+    "uint32":  ("u4", deserialize_number),
+    "int64":   ("i8", deserialize_number),
+    "uint64":  ("u8", deserialize_number),
     "float32": ("f4", deserialize_number),
     "float64": ("f8", deserialize_number),
-    "string": ("u1", deserialize_string),
-    "bool": ("u1", deserialize_number)
+    "string":  ("u1", deserialize_string),
+    "bool":    ("u1", deserialize_number)
 }
+
+
+
