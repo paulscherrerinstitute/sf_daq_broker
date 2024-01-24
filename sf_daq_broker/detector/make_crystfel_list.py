7465c2e6565cf9ce9d73e1e86c9aee95394a91dd
Sven Augustin <sven.augustin@psi.ch>
2024-01-24 13:15:56 +0100

    cleanup

--- sf_daq_broker/detector/make_crystfel_list.py
+++ sf_daq_broker/detector/make_crystfel_list.py
@@ -6,10 +6,12 @@ from time import sleep
 import h5py
 import numpy as np
 
+
 _logger = logging.getLogger("broker_writer")
 
-def make_crystfel_list(data_file, run_info_file, detector):
 
+
+def make_crystfel_list(data_file, run_info_file, detector):
     try:
         with open(run_info_file) as json_file:
             parameters = json.load(json_file)
@@ -18,17 +20,19 @@ def make_crystfel_list(data_file, run_info_file, detector):
         return
 
     try:
-        f=h5py.File(data_file, "r")
+        f = h5py.File(data_file, "r")
     except:
         _logger.error(f"Cannot open {data_file}")
         return
 
     pulseids = f[f"/data/{detector}/pulse_id"][:]
     n_pulse_id = len(pulseids)
-    if f"/data/{detector}/is_good_frame" in f.keys():
+
+    if f"/data/{detector}/is_good_frame" in f:
         is_good_frame = f[f"/data/{detector}/is_good_frame"][:]
     else:
         is_good_frame = [1] * n_pulse_id
+
     daq_recs = f[f"/data/{detector}/daq_rec"][:]
 
     nGoodFrames = 0
@@ -40,10 +44,12 @@ def make_crystfel_list(data_file, run_info_file, detector):
     for i in range(len(pulseids)):
         if not is_good_frame[i]:
             continue
+
         nGoodFrames += 1
         p = pulseids[i]
         nProcessedFrames += 1
         daq_rec = daq_recs[i]
+
         event_laser    = bool((daq_rec>>16)&1)
         event_darkshot = bool((daq_rec>>17)&1)
         event_fel      = bool((daq_rec>>18)&1)
@@ -64,24 +70,22 @@ def make_crystfel_list(data_file, run_info_file, detector):
 
     delim = "//"
 
-    if len(index_dark) > 0:
+    if index_dark:
         file_dark = data_file[:-3] + ".dark.lst"
         _logger.info(f"List of dark frames : {file_dark} , {len(index_dark)} frames")
-        f_list = open(file_dark, "w")
-        for frame_number in index_dark:
-            print(f"{data_file} //{frame_number}", file = f_list)
-        f_list.close()
+        with open(file_dark, "w") as f_list:
+            for frame_number in index_dark:
+                print(f"{data_file} {delim}{frame_number}", file = f_list)
 
-    if len(index_light) > 0:
+    if index_light:
         file_light = data_file[:-3] + ".light.lst"
         _logger.info(f"List of light frames : {file_light} , {len(index_light)} frames")
-        f_list = open(file_light, "w")
-        for frame_number in index_light:
-            print(f"{data_file} {delim}{frame_number}", file = f_list)
-        f_list.close()
+        with open(file_light, "w") as f_list:
+            for frame_number in index_light:
+                print(f"{data_file} {delim}{frame_number}", file = f_list)
+
 
 def store_dap_info(beamline=None, pgroup=None, detector=None, start_pulse_id=None, stop_pulse_id=None, file_name_out=None):
-
     if beamline is None or pgroup is None or detector is None or start_pulse_id is None or stop_pulse_id is None or file_name_out is None:
         _logger.error(f"input parameters to store_dap_info is not complete {beamline} {pgroup} {detector} {start_pulse_id} {stop_pulse_id} {file_name_out}")
         return
@@ -92,8 +96,7 @@ def store_dap_info(beamline=None, pgroup=None, detector=None, start_pulse_id=Non
         _logger.error(f"dap output is not reachable, may be dap is not working, path: {path_to_dap_files}")
         return
 
-    dap_ending = set([p//10000*10000 for p in range(start_pulse_id, stop_pulse_id+1)])
-
+    dap_ending = set([p // 10000 * 10000 for p in range(start_pulse_id, stop_pulse_id + 1)])
 
     sleep(10)
 
@@ -114,3 +117,6 @@ def store_dap_info(beamline=None, pgroup=None, detector=None, start_pulse_id=Non
                         n_lines_out += 1
 
     _logger.info(f"{n_lines_out} lines of dap output is stored in file {file_name_out}")
+
+
+
