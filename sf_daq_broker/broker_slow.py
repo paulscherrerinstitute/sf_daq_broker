c45d73099950a5cb016d45af1d760403231482c3
Sven Augustin <sven.augustin@psi.ch>
2024-01-24 11:34:18 +0100

    cleanup

--- sf_daq_broker/broker_slow.py
+++ sf_daq_broker/broker_slow.py
@@ -7,15 +7,15 @@ import bottle
 from sf_daq_broker.broker_manager_slow import DetectorManager
 from sf_daq_broker.rest_api_slow import register_rest_interface
 
+
 _logger = logging.getLogger(__name__)
 
 
-def start_server(rest_port):
 
+def start_server(rest_port):
     _logger.info(f"Starting detector server on port {rest_port} (rest-api)")
 
     app = bottle.Bottle()
-
     manager = DetectorManager()
 
     logging.getLogger("pika").setLevel(logging.WARNING)
@@ -26,25 +26,28 @@ def start_server(rest_port):
 
     hostname = socket.gethostname()
     _logger.info(f"Starting rest API on port {rest_port} host {hostname}" )
+
     bottle.run(app=app, host=hostname, port=rest_port)
 
 
 def run():
     parser = argparse.ArgumentParser(description="detector_settings")
 
-    parser.add_argument("--rest_port", type=int, help="Port for REST api.", default=10003)
+    parser.add_argument("--rest_port", default=10003, type=int, help="Port for REST api.")
+    parser.add_argument("--log_level", default="INFO", choices=["CRITICAL", "ERROR", "WARNING", "INFO", "DEBUG"], help="Log level to use.")
 
-    parser.add_argument("--log_level", default="INFO",
-                        choices=["CRITICAL", "ERROR", "WARNING", "INFO", "DEBUG"],
-                        help="Log level to use.")
+    clargs = parser.parse_args()
 
-    arguments = parser.parse_args()
+    logging.basicConfig(level=clargs.log_level, format="[%(levelname)s] %(message)s")
 
-    # Setup the logging level.
-    logging.basicConfig(level=arguments.log_level, format="[%(levelname)s] %(message)s")
+    start_server(rest_port=clargs.rest_port)
 
-    start_server(rest_port=arguments.rest_port)
+
+
 
 
 if __name__ == "__main__":
     run()
+
+
+
