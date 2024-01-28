dc70a267132fa0fa7f68ee06e54137c335390330
Sven Augustin <sven.augustin@psi.ch>
2024-01-28 22:34:45 +0100

    cleanup

--- sf_daq_broker/writer/start.py
+++ sf_daq_broker/writer/start.py
@@ -17,13 +17,14 @@ from sf_daq_broker.utils import get_data_api_request, get_writer_request
 from sf_daq_broker.writer.bsread_writer import write_from_databuffer_api3, write_from_imagebuffer
 from sf_daq_broker.writer.detector_writer import detector_retrieve
 
+
 _logger = logging.getLogger("broker_writer")
 
 
-def audit_failed_write_request(write_request):
 
+def audit_failed_write_request(write_request):
     original_output_file = write_request.get("output_file", None)
-    if original_output_file is None:
+    if not original_output_file:
         return
 
     output_file = original_output_file + ".err"
@@ -40,7 +41,6 @@ def audit_failed_write_request(write_request):
 
 
 def wait_for_delay(request_timestamp, writer_type):
-
     if request_timestamp is None:
         return
 
@@ -48,24 +48,21 @@ def wait_for_delay(request_timestamp, writer_type):
     if writer_type == broker_config.TAG_DETECTOR_RETRIEVE:
         time_to_wait = config.DETECTOR_RETRIEVAL_DELAY
 
-# should not come here in this case, since request_timestamp is None
+#    # should not come here in this case, since request_timestamp is None
 #    if writer_type == broker_config.TAG_PEDESTAL or writer_type != broker_config.TAG_POWER_ON:
 #        time_to_wait = 0
 
     current_timestamp = time()
     # sleep time = target sleep time - time that has already passed.
     adjusted_retrieval_delay = time_to_wait - (current_timestamp - request_timestamp)
-
     adjusted_retrieval_delay = max(adjusted_retrieval_delay, 0)
 
     _logger.debug(f"Request timestamp={request_timestamp}, current_timestamp={current_timestamp}, adjusted_retrieval_delay={adjusted_retrieval_delay}.")
-
     _logger.info(f"Sleeping for {adjusted_retrieval_delay} seconds before continuing.")
     sleep(adjusted_retrieval_delay)
 
 
 def process_request(request, broker_client):
-
     writer_type = request["writer_type"]
     channels = request.get("channels", None)
 
@@ -85,13 +82,8 @@ def process_request(request, broker_client):
         _logger.addHandler(file_handler)
 
         logger_data_api = None
-
-        if writer_type == broker_config.TAG_DATA3BUFFER:
+        if writer_type in (broker_config.TAG_DATA3BUFFER, broker_config.TAG_IMAGEBUFFER):
             logger_data_api = logging.getLogger("data_api3")
-        elif writer_type == broker_config.TAG_IMAGEBUFFER:
-            logger_data_api = logging.getLogger("data_api3")
-
-        if logger_data_api is not None:
             logger_data_api.addHandler(file_handler)
 
     try:
@@ -123,28 +115,31 @@ def process_request(request, broker_client):
             _logger.info("Doing pedestal.")
 
             detectors = request.get("detectors", [])
-            det_start_pulse_id, det_stop_pulse_id = take_pedestal(detectors_name=detectors, rate=request.get("rate_multiplicator", 1))
+            rate_multiplicator = request.get("rate_multiplicator", 1)
+            det_start_pulse_id, det_stop_pulse_id = take_pedestal(detectors_name=detectors, rate=rate_multiplicator)
 
             # overwrite start/stop pulse_ids in run_info json file
             run_file_json = request.get("run_file_json", None)
             if run_file_json is not None:
                 with open(run_file_json, "r") as request_json_file:
                     run_info = json.load(request_json_file)
+
                 run_info["start_pulseid"] = det_start_pulse_id
                 run_info["stop_pulseid"]  = det_stop_pulse_id
+
                 with open(run_file_json, "w") as request_json_file:
                     json.dump(run_info, request_json_file, indent=2)
 
             request_det_retrieve = {
-                                     "det_start_pulse_id" : det_start_pulse_id,
-                                     "det_stop_pulse_id"  : det_stop_pulse_id,
-                                     "rate_multiplicator" : request.get("rate_multiplicator", 1),
-                                     "run_file_json"      : request.get("run_file_json", None),
-                                     "path_to_pgroup"     : request.get("path_to_pgroup", None),
-                                     "run_info_directory" : request.get("run_info_directory", None),
-                                     "directory_name"     : request.get("directory_name"),
-                                     "request_time"       : request.get("request_time", str(datetime.now()))
-                                   }
+                "det_start_pulse_id" : det_start_pulse_id,
+                "det_stop_pulse_id"  : det_stop_pulse_id,
+                "rate_multiplicator" : request.get("rate_multiplicator", 1),
+                "run_file_json"      : request.get("run_file_json", None),
+                "path_to_pgroup"     : request.get("path_to_pgroup", None),
+                "run_info_directory" : request.get("run_info_directory", None),
+                "directory_name"     : request.get("directory_name"),
+                "request_time"       : request.get("request_time", str(datetime.now()))
+            }
 
             broker_client.open()
 
@@ -152,17 +147,20 @@ def process_request(request, broker_client):
                 request_det_retrieve["detector_name"] = detector
                 request_det_retrieve["detectors"] = {}
                 request_det_retrieve["detectors"][detector] = {}
+
                 output_file_prefix = request.get("output_file_prefix", "/tmp/error")
                 output_file_det = f"{output_file_prefix}.{detector}.h5"
                 run_log_file_det = run_log_file[:-4] + "." + detector + ".log"
 
-                write_request = get_writer_request(writer_type=broker_config.TAG_DETECTOR_RETRIEVE,
-                                                   channels=request_det_retrieve,
-                                                   output_file=output_file_det,
-                                                   metadata=None,
-                                                   start_pulse_id=det_start_pulse_id,
-                                                   stop_pulse_id=det_start_pulse_id,
-                                                   run_log_file=run_log_file_det)
+                write_request = get_writer_request(
+                    writer_type=broker_config.TAG_DETECTOR_RETRIEVE,
+                    channels=request_det_retrieve,
+                    output_file=output_file_det,
+                    metadata=None,
+                    start_pulse_id=det_start_pulse_id,
+                    stop_pulse_id=det_start_pulse_id,
+                    run_log_file=run_log_file_det
+                )
 
                 broker_client.send(write_request, broker_config.TAG_DETECTOR_RETRIEVE)
 
@@ -170,7 +168,9 @@ def process_request(request, broker_client):
 
         elif writer_type == broker_config.TAG_POWER_ON:
             _logger.info("Power ON detector")
-            power_on_detector(detector_name=request.get("detector_name", None), beamline=request.get("beamline", None))
+            detector_name = request.get("detector_name", None)
+            beamline = request.get("beamline", None)
+            power_on_detector(detector_name=detector_name, beamline=beamline)
 
         elif writer_type == broker_config.TAG_DETECTOR_RETRIEVE:
             _logger.info("Using detector retrieve writer.")
@@ -178,17 +178,20 @@ def process_request(request, broker_client):
 
         elif writer_type == broker_config.TAG_DETECTOR_CONVERT:
             _logger.info("Using detector convert writer.")
+            #TODO: nothing here?
 
         delta_time = time() - start_time
         _logger.info(f"Finished. Took {delta_time} seconds to complete request.")
 
-        if file_handler:
-            _logger.removeHandler(file_handler)
-            if logger_data_api is not None:
-                logger_data_api.removeHandler(file_handler)
+#        #TODO this block is also in finally, it will run there anyway
+#        if file_handler:
+#            _logger.removeHandler(file_handler)
+#            if logger_data_api is not None:
+#                logger_data_api.removeHandler(file_handler)
 
     except Exception:
         audit_failed_write_request(request)
+        _logger.exception(f"Request failed: {request}")
         raise
 
     finally:
@@ -199,7 +202,6 @@ def process_request(request, broker_client):
 
 
 def update_status(channel, body, action, file, message=None):
-
     status_header = {
         "action": action,
         "source": "bsread_writer",
@@ -208,28 +210,25 @@ def update_status(channel, body, action, file, message=None):
         "message": message
     }
 
-    channel.basic_publish(exchange=broker_config.STATUS_EXCHANGE,
-                          properties=BasicProperties(
-                              headers=status_header),
-                          routing_key="",
-                          body=body)
+    channel.basic_publish(
+        exchange=broker_config.STATUS_EXCHANGE,
+        properties=BasicProperties(headers=status_header),
+        routing_key="",
+        body=body
+    )
 
 
 def confirm_request(channel, method_frame, body, output_file):
     channel.basic_ack(delivery_tag=method_frame.delivery_tag)
-
     update_status(channel, body, "write_finished", output_file)
 
 
 def reject_request(channel, method_frame, body, output_file, e):
-    channel.basic_reject(delivery_tag=method_frame.delivery_tag,
-                         requeue=False)
-
+    channel.basic_reject(delivery_tag=method_frame.delivery_tag, requeue=False)
     update_status(channel, body, "write_rejected", output_file, str(e))
 
 
 def on_broker_message(channel, method_frame, _header_frame, body, connection, broker_client):
-
     try:
         request = json.loads(body.decode())
 
@@ -240,15 +239,14 @@ def on_broker_message(channel, method_frame, _header_frame, body, connection, br
             try:
                 process_request(request, broker_client)
 
-            except Exception as ex:
+            except Exception as e:
                 _logger.exception("Error while trying to write a requested data.")
-
-                reject_request_f = partial(reject_request, channel, method_frame, body, output_file, ex)
-                connection.add_callback_threadsafe(reject_request_f)
+                callback = partial(reject_request, channel, method_frame, body, output_file, e)
 
             else:
-                confirm_request_f = partial(confirm_request, channel, method_frame, body, output_file)
-                connection.add_callback_threadsafe(confirm_request_f)
+                callback = partial(confirm_request, channel, method_frame, body, output_file)
+
+            connection.add_callback_threadsafe(callback)
 
         thread = Thread(target=process_async)
         thread.daemon = True
@@ -260,38 +258,37 @@ def on_broker_message(channel, method_frame, _header_frame, body, connection, br
 
 
 def start_service(broker_url, writer_type=0):
-
     connection = BlockingConnection(ConnectionParameters(broker_url))
     channel = connection.channel()
 
-    channel.exchange_declare(exchange=broker_config.STATUS_EXCHANGE,
-                             exchange_type="fanout")
-    channel.exchange_declare(exchange=broker_config.REQUEST_EXCHANGE,
-                             exchange_type="topic")
+    channel.exchange_declare(exchange=broker_config.STATUS_EXCHANGE,  exchange_type="fanout")
+    channel.exchange_declare(exchange=broker_config.REQUEST_EXCHANGE, exchange_type="topic")
 
-    routing_key   = broker_config.DEFAULT_ROUTE
-    request_queue = broker_config.DEFAULT_QUEUE
-    if writer_type == 1:
-        routing_key   = broker_config.DETECTOR_RETRIEVE_ROUTE
-        request_queue = broker_config.DETECTOR_RETRIEVE_QUEUE
-    if writer_type == 2:
-        routing_key   = broker_config.DETECTOR_CONVERSION_ROUTE
-        request_queue = broker_config.DETECTOR_CONVERSION_QUEUE
-    if writer_type == 3:
-        routing_key   = broker_config.DETECTOR_PEDESTAL_ROUTE
-        request_queue = broker_config.DETECTOR_PEDESTAL_QUEUE
+    #TODO: should introduce named writer_types / enum?
+
+    ROUTING_KEYS = {
+        1: broker_config.DETECTOR_RETRIEVE_ROUTE,
+        2: broker_config.DETECTOR_CONVERSION_ROUTE,
+        3: broker_config.DETECTOR_PEDESTAL_ROUTE
+    }
+
+    REQUEST_QUEUES = {
+        1: broker_config.DETECTOR_RETRIEVE_QUEUE,
+        2: broker_config.DETECTOR_CONVERSION_QUEUE,
+        3: broker_config.DETECTOR_PEDESTAL_QUEUE
+    }
+
+    routing_key   = ROUTING_KEYS.get(writer_type, broker_config.DEFAULT_ROUTE)
+    request_queue = REQUEST_QUEUES.get(writer_type, broker_config.DEFAULT_QUEUE)
 
     channel.queue_declare(queue=request_queue, auto_delete=True)
-    channel.queue_bind(queue=request_queue,
-                       exchange=broker_config.REQUEST_EXCHANGE,
-                       routing_key=routing_key)
-
+    channel.queue_bind(queue=request_queue, exchange=broker_config.REQUEST_EXCHANGE, routing_key=routing_key)
     channel.basic_qos(prefetch_count=1)
 
     broker_client = RabbitMqClient(broker_url=broker_url)
 
-    on_broker_message_f = partial(on_broker_message, connection=connection, broker_client=broker_client)
-    channel.basic_consume(request_queue, on_broker_message_f)
+    on_broker_message_cb = partial(on_broker_message, connection=connection, broker_client=broker_client)
+    channel.basic_consume(request_queue, on_broker_message_cb)
 
     try:
         channel.start_consuming()
@@ -302,41 +299,39 @@ def start_service(broker_url, writer_type=0):
 def run():
     parser = argparse.ArgumentParser(description="data writer")
 
-    parser.add_argument("--broker_url", default=broker_config.DEFAULT_BROKER_URL,
-                        help="Address of the broker to connect to.")
-    parser.add_argument("--log_level", default="INFO",
-                        choices=["CRITICAL", "ERROR", "WARNING", "INFO", "DEBUG"],
-                        help="Log level to use.")
-    parser.add_argument("--writer_id", default=1, type=int,
-                        help="Id of the writer")
-    parser.add_argument("--writer_type", default=0, type=int,
-                        help="Type of the writer (0-epics/bs/camera; 1 - detector retrieve; 2 - detector conversion; 3 - pedestal)")
+    parser.add_argument("--broker_url", default=broker_config.DEFAULT_BROKER_URL, help="Address of the broker to connect to.")
+    parser.add_argument("--log_level", default="INFO", choices=["CRITICAL", "ERROR", "WARNING", "INFO", "DEBUG"], help="Log level to use.")
+    parser.add_argument("--writer_id", default=1, type=int, help="Id of the writer")
+    parser.add_argument("--writer_type", default=0, type=int, help="Type of the writer (0-epics/bs/camera; 1 - detector retrieve; 2 - detector conversion; 3 - pedestal)")
 
-    args = parser.parse_args()
-
-# Logging formating:
-    _logger.setLevel(args.log_level)
+    clargs = parser.parse_args()
 
     stream_handler = logging.StreamHandler()
-    stream_handler.setLevel(args.log_level)
-    formatter = logging.Formatter(f"[%(levelname)s] (broker_writer_{args.writer_id}_{args.writer_type}) %(message)s")
+    stream_handler.setLevel(clargs.log_level)
+    formatter = logging.Formatter(f"[%(levelname)s] (broker_writer_{clargs.writer_id}_{clargs.writer_type}) %(message)s")
     stream_handler.setFormatter(formatter)
 
-    _logger.setLevel(args.log_level)
+    _logger.setLevel(clargs.log_level)
     _logger.addHandler(stream_handler)
 
     for logger_name in ["data_api", "data_api3"]:
         logger_data_api = logging.getLogger(logger_name)
-        logger_data_api.setLevel(args.log_level)
+        logger_data_api.setLevel(clargs.log_level)
         logger_data_api.addHandler(stream_handler)
 
-# make message-broker less noisy in logs
+    # make message-broker less noisy in logs
     logging.getLogger("pika").setLevel(logging.WARNING)
 
     _logger.info("Writer started. Waiting for requests.")
 
-    start_service(broker_url=args.broker_url, writer_type=args.writer_type)
+    start_service(broker_url=clargs.broker_url, writer_type=clargs.writer_type)
+
+
+
 
 
 if __name__ == "__main__":
     run()
+
+
+
