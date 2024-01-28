11b2c300aa2066dac9b49576add0ff83a79b0554
Sven Augustin <sven.augustin@psi.ch>
2024-01-28 23:19:14 +0100

    cleanup

--- sf_daq_broker/writer/postprocess_raw.py
+++ sf_daq_broker/writer/postprocess_raw.py
@@ -6,9 +6,14 @@ import h5py
 import numpy as np
 from bitshuffle.h5 import H5_COMPRESS_LZ4, H5FILTER  # pylint: disable=no-name-in-module
 
+
 # bitshuffle hdf5 filter params
 BLOCK_SIZE = 2048
-compargs = {"compression": H5FILTER, "compression_opts": (BLOCK_SIZE, H5_COMPRESS_LZ4)}
+COMPARGS = {
+    "compression": H5FILTER,
+    "compression_opts": (BLOCK_SIZE, H5_COMPRESS_LZ4)
+}
+
 # limit bitshuffle omp to a single thread
 # a better fix would be to use bitshuffle compiled without omp support
 os.environ["OMP_NUM_THREADS"] = "1"
@@ -20,9 +25,9 @@ MODULE_SIZE_X = 1024
 MODULE_SIZE_Y = 512
 
 
-def postprocess_raw(
-    source, dest, disabled_modules=(), index=None, compression=False, batch_size=100
-):
+
+def postprocess_raw(source, dest, disabled_modules=(), index=None, compression=False, batch_size=100):
+    #TODO: this relies on h5_dest, which is only opened below
     # a function for "visititems" should have the args (name, object)
     def _visititems(name, obj):
         if isinstance(obj, h5py.Group):
@@ -40,8 +45,7 @@ def postprocess_raw(
                     else:
                         data = dset_source[index, :]
 
-                    args = {"shape": data.shape}
-                    h5_dest.create_dataset_like(name, dset_source, data=data, **args)
+                    h5_dest.create_dataset_like(name, dset_source, data=data, shape=data.shape)
                 else:
                     h5_dest.create_dataset_like(name, dset_source, data=dset_source)
 
@@ -63,7 +67,6 @@ def postprocess_raw(
         # now process the raw data
         dset = h5_source[data_dset]
 
-        args = {}
         if index is None:
             n_images = dset.shape[0]
         else:
@@ -73,12 +76,14 @@ def postprocess_raw(
         n_modules = dset.shape[1] // MODULE_SIZE_Y
         out_shape = (MODULE_SIZE_Y * (n_modules - len(disabled_modules)), MODULE_SIZE_X)
 
-        args["shape"] = (n_images, *out_shape)
-        args["maxshape"] = (n_images, *out_shape)
-        args["chunks"] = (1, *out_shape)
+        args = {
+            "shape"    : (n_images, *out_shape),
+            "maxshape" : (n_images, *out_shape),
+            "chunks"   : (1,        *out_shape)
+        }
 
         if compression:
-            args.update(compargs)
+            args.update(COMPARGS)
 
         h5_dest.create_dataset_like(data_dset, dset, **args)
 
@@ -96,11 +101,12 @@ def postprocess_raw(
 
         # prepare buffers to be reused for every batch
         read_buffer = np.empty((batch_size, *dset.shape[1:]), dtype=DTYPE)
-        out_buffer = np.zeros((batch_size, *out_shape), dtype=DTYPE)
+        out_buffer  = np.zeros((batch_size, *out_shape),      dtype=DTYPE)
 
         # process and write data in batches
         for batch_start_ind in range(0, n_images, batch_size):
-            batch_range = range(batch_start_ind, min(batch_start_ind + batch_size, n_images))
+            batch_stop_ind = min(batch_start_ind + batch_size, n_images)
+            batch_range = range(batch_start_ind, batch_stop_ind)
 
             if index is None:
                 batch_ind = np.array(batch_range)
@@ -109,22 +115,31 @@ def postprocess_raw(
 
             #TODO: avoid unnecessary buffers
             read_buffer_view = read_buffer[: len(batch_ind)]
-            out_buffer_view = out_buffer[: len(batch_ind)]
+            out_buffer_view  = out_buffer[: len(batch_ind)]
 
             # Avoid a stride-bottleneck, see https://github.com/h5py/h5py/issues/977
             if np.sum(np.diff(batch_ind)) == len(batch_ind) - 1:
                 # consecutive index values
-                dset.read_direct(read_buffer_view, source_sel=np.s_[batch_ind])
+                sel = np.s_[batch_ind]
+                dset.read_direct(read_buffer_view, source_sel=sel)
             else:
                 for i, j in enumerate(batch_ind):
-                    dset.read_direct(read_buffer_view, source_sel=np.s_[j], dest_sel=np.s_[i])
+                    sel_i = np.s_[i]
+                    sel_j = np.s_[j]
+                    dset.read_direct(read_buffer_view, source_sel=sel_j, dest_sel=sel_i)
 
             for i, m in enumerate(module_map):
                 if m == -1:
                     continue
 
-                read_slice = read_buffer_view[:, i * MODULE_SIZE_Y : (i + 1) * MODULE_SIZE_Y, :]
-                out_slice = out_buffer_view[:, m * MODULE_SIZE_Y : (m + 1) * MODULE_SIZE_Y, :]
+                istart = i * MODULE_SIZE_Y
+                mstart = m * MODULE_SIZE_Y
+
+                istop = (i + 1) * MODULE_SIZE_Y
+                mstop = (m + 1) * MODULE_SIZE_Y
+
+                read_slice = read_buffer_view[:, istart:istop, :]
+                out_slice = out_buffer_view[:, mstart:mstop, :]
                 out_slice[:] = read_slice
 
             bytes_num_elem = struct.pack(">q", out_shape[0] * out_shape[1] * DTYPE_SIZE)
@@ -138,3 +153,6 @@ def postprocess_raw(
                     byte_array = im.tobytes()
 
                 h5_dest[data_dset].id.write_direct_chunk((pos, 0, 0), byte_array)
+
+
+
