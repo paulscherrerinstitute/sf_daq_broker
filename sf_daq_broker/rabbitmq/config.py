0dc9959a27561473a34094f86f17098c9de0691c
Sven Augustin <sven.augustin@psi.ch>
2024-01-29 20:03:34 +0100

    alphabetically sorted

--- sf_daq_broker/rabbitmq/config.py
+++ sf_daq_broker/rabbitmq/config.py
@@ -9,20 +9,20 @@ STATUS_EXCHANGE = "status"
 DEFAULT_QUEUE = "write_request"
 DEFAULT_ROUTE = "bs.*"
 
-DETECTOR_RETRIEVE_QUEUE = "detector_retrieve"
-DETECTOR_RETRIEVE_ROUTE = "detector.retrieve"
-
 DETECTOR_CONVERSION_QUEUE = "detector_convert"
 DETECTOR_CONVERSION_ROUTE = "detector.convert"
 
 DETECTOR_PEDESTAL_QUEUE = "detector_pedestal"
 DETECTOR_PEDESTAL_ROUTE = "detector.pedestal"
 
+DETECTOR_RETRIEVE_QUEUE = "detector_retrieve"
+DETECTOR_RETRIEVE_ROUTE = "detector.retrieve"
+
 # Name of the tags for rabbitmq requests.
-TAG_IMAGEBUFFER = "imagebuffer"
 TAG_DATA3BUFFER = "data3buffer"
-TAG_PEDESTAL = "pedestal"
-TAG_DETECTOR_RETRIEVE = "detector_buffer"
 TAG_DETECTOR_CONVERT  = "detector_convert"
+TAG_DETECTOR_RETRIEVE = "detector_buffer"
+TAG_IMAGEBUFFER = "imagebuffer"
+TAG_PEDESTAL = "pedestal"
 TAG_POWER_ON = "detector_power_on"
 
