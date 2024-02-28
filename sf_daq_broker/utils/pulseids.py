fe88719e0e5dc5df330fe275fa06c0851120e55e
Sven Augustin <sven.augustin@psi.ch>
2024-02-28 11:47:33 +0100

    use exceptions for error handling

--- sf_daq_broker/utils/pulseids.py
+++ sf_daq_broker/utils/pulseids.py
@@ -4,6 +4,7 @@ from time import sleep
 import requests
 
 from sf_daq_broker import config
+from .excfmt import dueto
 
 
 _logger = logging.getLogger("broker_writer")
@@ -19,17 +20,21 @@ def pulse_id_to_timestamp(pulse_id, n_tries=3, wait_time=5):
     url = f"{config.PULSEID2SECONDS_MATCHING_ADDRESS}/{pulse_id}"
 
     for i in range(n_tries):
-        response = requests.get(url)
+        try:
+            return request_get(url)
+        except Exception as e:
+            _logger.warning(f"mapping pulse ID {pulse_id} to timestamp failed #{i+1}/{n_tries} {dueto(e)} -- will try again in {wait_time} seconds")
+            sleep(wait_time)
 
-        if response.status_code == 200:
-            return response.json()
-
-        _logger.warning(f"mapping pulse ID {pulse_id} to timestamp failed #{i+1}/{n_tries} -- will try again in {wait_time} seconds")
-        sleep(wait_time)
-
-    msg = f"mapping pulse ID {pulse_id} to timestamp failed {n_tries} times: {response}"
+    msg = f"mapping pulse ID {pulse_id} to timestamp failed {n_tries} times"
     _logger.error(msg)
     raise RuntimeError(msg)
+
+
+def request_get(url):
+    response = requests.get(url)
+    response.raise_for_status()
+    return response.json()
 
 
 
