707535428611d1c3e2fae74ede3a1281ac47b01b
NichtJens <nichtjens@gmail.com>
2024-03-03 12:30:07 +0100

    less self

--- tests/test_broker.py
+++ tests/test_broker.py
@@ -11,14 +11,15 @@ from sf_daq_broker import broker
 class TestBroker(unittest.TestCase):
 
     def setUp(self):
-        self.broker_url = None
-        self.rest_port = 11000
+        broker_url = None
+        rest_port = 11000
 
         # bottle server binds to this hostname
-        self.hostname = socket.gethostname()
+        hostname = socket.gethostname()
+        self.address = f"http://{hostname}:{rest_port}"
 
         self.broker_process = Process(
-            target=broker.start_server, args=(self.broker_url, self.rest_port)
+            target=broker.start_server, args=(broker_url, rest_port)
         )
 
         self.broker_process.start()
@@ -32,7 +33,7 @@ class TestBroker(unittest.TestCase):
 
     def test_REST_API(self):
         # test response status code for non-existent endpoint
-        url = f"http://{self.hostname}:{self.rest_port}/this_endpoint_does_not_exist"
+        url = f"{self.address}/this_endpoint_does_not_exist"
         response = requests.get(url)
         self.assertEqual(response.status_code, 404)
 
@@ -45,7 +46,7 @@ class TestBroker(unittest.TestCase):
             "get_pvlist"
         ]
         for ep in get_endpoints:
-            response = requests.get(f"http://{self.hostname}:{self.rest_port}/{ep}")
+            response = requests.get(f"{self.address}/{ep}")
             status = response.json()["status"]
             self.assertIn(status, ["ok", "failed"])
 
