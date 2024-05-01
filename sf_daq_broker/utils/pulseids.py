dde70bb4295edd59c91ad82960a963cb4740101d
NichtJens <nichtjens@gmail.com>
2024-05-01 20:16:58 +0200

    try some more; add linear backoff

--- sf_daq_broker/utils/pulseids.py
+++ sf_daq_broker/utils/pulseids.py
@@ -16,15 +16,16 @@ def pulse_id_to_seconds(pulse_id, **kwargs):
     return ts / 1e9
 
 
-def pulse_id_to_timestamp(pulse_id, n_tries=3, wait_time=5):
+def pulse_id_to_timestamp(pulse_id, n_tries=15, wait_time=5):
     url = f"{config.PULSEID2SECONDS_MATCHING_ADDRESS}/{pulse_id}"
 
     for i in range(n_tries):
+        count = i + 1
         try:
             return request_get(url)
         except Exception as e:
-            _logger.warning(f"mapping pulse ID {pulse_id} to timestamp failed #{i+1}/{n_tries} {dueto(e)} -- will try again in {wait_time} seconds")
-            sleep(wait_time)
+            _logger.warning(f"mapping pulse ID {pulse_id} to timestamp failed #{count}/{n_tries} {dueto(e)} -- will try again in {wait_time * count} seconds")
+            sleep(wait_time * count)
 
     msg = f"mapping pulse ID {pulse_id} to timestamp failed {n_tries} times"
     _logger.error(msg)
