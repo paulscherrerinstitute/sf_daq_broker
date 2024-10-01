42329b7585cbf53db77dc2810df43a9c37aaeb48
NichtJens <nichtjens@gmail.com>
2024-10-01 19:29:43 +0200

    cleanup of description and help texts; added short switches; fill attribute dict then update attrs

--- client/jungfrau_convert_gainmaps.py
+++ client/jungfrau_convert_gainmaps.py
@@ -11,44 +11,46 @@ GAINS = ("G0", "G1", "G2", "HG0", "HG1", "HG2")
 MODULE_SHAPE = (6, 512, 1024)
 MODULE_PIXELS = 512 * 1024
 
+PRINTABLE_GAINS = ", ".join(GAINS)
+
 
 def main():
-    parser = argparse.ArgumentParser(description="""
-        Utility to read binary Jungfrau gain maps from PSI Detectors Group and save them in a 3d array in an hdf5 file.
-        Gain maps are saved in a 3d array (with first index being G0, G1, G2), in the geometry specified by the optional argument --geometry. Some standard attributes are filled by default, like "date", "creator", "original_filenames".
-        The name of the dataset used for saving the maps is "gains".
-    """)
+    parser = argparse.ArgumentParser(description=f'''
+        Utility to read binary Jungfrau gain maps from the PSI Detector Group and write them as 3D array to a hdf5 file.
+        The first axis in the 3D array is {PRINTABLE_GAINS}.
+        The name of the dataset used for saving the maps is "{DST_NAME}".
+        Some attributes are filled by default: "creator", "date", "original_filenames".
+    ''')
 
-    parser.add_argument("files", metavar="file", nargs="+", help="Binary file as provided by the PSI Detector Group, usually one per module. Order matters! The first file provided is the bottom-left module.")
-    parser.add_argument("--outfile", default="gains.h5", help="Name for the hdf5 output file")
-    parser.add_argument("--attributes", nargs="+", default="", help="Additional attributes to be added to the destination dataset, in the form: key=value key=value ...")
-    parser.add_argument("--shape", type=int, nargs=2, help="Dimension of the final image, in modules. For example, a 1.5 Jungfrau with three modules one on top of each other is: 3 1.")
+    parser.add_argument("fnames", metavar="file", nargs="+", help="Binary files as provided by the PSI Detector Group, usually one per module. Order matters! The first file provided is the bottom-left module.")
+    parser.add_argument("-o", "--output", default="gains.h5", help="Name for the hdf5 output file")
+    parser.add_argument("-a", "--attributes", metavar="ATTRIBUTE", nargs="+", default="", help="Additional attributes to be added to the destination dataset, in the form: key=value")
+    parser.add_argument("-s", "--shape", metavar=("HEIGHT", "WIDTH"), type=int, nargs=2, help="Dimensions of the final image, in number of modules. For example, a 1.5M Jungfrau with three modules one on top of each other is: 3 1. Defaults to stacking vertically, if nothing is provided.")
 
     clargs = parser.parse_args()
 
-    if clargs.shape is None:
-        n_modules = len(clargs.files)
-        clargs.shape = (n_modules, 1)
+    fnames = clargs.fnames
+    fn_out = clargs.output
+    attributes = dict(kv.split("=") for kv in clargs.attributes)
 
-    modules = [load_module(i, fn) for i, fn in enumerate(clargs.files)]
+    shape = clargs.shape
+    if shape is None:
+        n_modules = len(fnames)
+        shape = (n_modules, 1)
 
-    data = merge_gainmaps(modules, clargs.shape, MODULE_SHAPE)
+    attributes["creator"] = os.getenv("USER")
+    attributes["date"] = datetime.now().strftime("%Y%m%d %H:%M:%S")
+    attributes["original_filenames"] = " ".join(fnames)
 
-    with h5py.File(clargs.outfile, "w") as h5f:
+    modules = [load_module(i, fn) for i, fn in enumerate(fnames)]
+    data = merge_gainmaps(modules, shape, MODULE_SHAPE)
+
+    with h5py.File(fn_out, "w") as h5f:
         dst = h5f.create_dataset(DST_NAME, data=data)
-
-        dst.attrs["creator"] = os.getenv("USER")
-        dst.attrs["date"] = datetime.now().strftime("%Y%m%d %H:%M:%S")
-        dst.attrs["original_filenames"] = " ".join(clargs.files)
-
-        for kv in clargs.attributes:
-            if kv == "":
-                continue
-            key, val = kv.split("=")
-            dst.attrs[key] = val
+        dst.attrs.update(attributes)
 
         print()
-        print(f"File {clargs.outfile} written, size of {DST_NAME} dataset: {dst.shape}")
+        print(f"File {fn_out} written, size of {DST_NAME} dataset: {dst.shape}")
         print("Written attributes:")
         for k, v in dst.attrs.items():
             print(f"\t{k}: {v}")
