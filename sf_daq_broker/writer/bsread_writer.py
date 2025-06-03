a3f51f6611f19e85c447ddf890b1de6b7b976187
NichtJens <nichtjens@gmail.com>
2025-06-03 22:16:25 +0200

    renamed matched_pulse_id -> matched_pulse_ids

--- sf_daq_broker/writer/bsread_writer.py
+++ sf_daq_broker/writer/bsread_writer.py
@@ -119,24 +119,24 @@ def check_data_consistency(start_pulse_id, stop_pulse_id, rate_multiplicator, ch
                 n_duplicates = n_raw_pulse_ids - n_raw_pulse_ids_unique
                 _logger.error(f"check {channel} contains duplicate entries: total {n_raw_pulse_ids}, duplicates {n_duplicates}")
 
-            matched_pulse_id = np.intersect1d(expected_pulse_ids, raw_pulse_ids)
-            n_matched_pulse_id = len(matched_pulse_id)
+            matched_pulse_ids = np.intersect1d(expected_pulse_ids, raw_pulse_ids)
+            n_matched_pulse_id = len(matched_pulse_ids)
 
             if n_matched_pulse_id != n_expected_pulse_ids:
                 _logger.error(f"check {channel} number of (unique) pulse IDs {n_matched_pulse_id} differs from requested {n_expected_pulse_ids}")
 
-            start_matched_pulse_id = matched_pulse_id[0] if len(matched_pulse_id) else None
+            start_matched_pulse_id = matched_pulse_ids[0] if len(matched_pulse_ids) else None
             start_expected = expected_pulse_ids[0]
             if start_matched_pulse_id != start_expected:
                 _logger.error(f"check {channel} start pulse ID {start_matched_pulse_id} differs from requested {start_expected}")
 
-            stop_matched_pulse_id = matched_pulse_id[-1] if len(matched_pulse_id) else None
+            stop_matched_pulse_id = matched_pulse_ids[-1] if len(matched_pulse_ids) else None
             stop_expected = expected_pulse_ids[-1]
             if stop_matched_pulse_id != stop_expected:
                 _logger.error(f"check {channel} stop pulse ID {stop_matched_pulse_id} differs from requested {stop_expected}")
 
             if start_matched_pulse_id == start_expected and stop_matched_pulse_id == stop_expected and not has_duplicates:
-                for pi, epi in zip(matched_pulse_id, expected_pulse_ids):
+                for pi, epi in zip(matched_pulse_ids, expected_pulse_ids):
                     if pi != epi:
                         _logger.error(f"check {channel} pulse IDs are not monotonic: {pi} != {epi}")
                         break
