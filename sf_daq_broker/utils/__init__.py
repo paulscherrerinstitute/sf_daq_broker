058cb2f273b0f799397ddf9b099cafb29e4b3091
NichtJens <nichtjens@gmail.com>
2025-10-30 15:49:49 +0100

    added GitRepo

--- sf_daq_broker/utils/__init__.py
+++ sf_daq_broker/utils/__init__.py
@@ -3,6 +3,7 @@ from .detname import parse_det_name
 from .excfmt import dueto, excfmt, typename
 from .get_beamline import get_beamline
 from .get_requests import get_data_api_request, get_writer_request
+from .gitrepo import GitRepo
 from .jsonext import json_load, json_save, json_obj_to_str, json_str_to_obj
 from .load_module import load_module
 from .ping import ping, ping_many
