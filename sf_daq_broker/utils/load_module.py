7eaaaf1bad2a48cbfe524ae5dc28b8e40c24a18e
NichtJens <nichtjens@gmail.com>
2025-11-01 00:11:37 +0100

    changed Path import

--- sf_daq_broker/utils/load_module.py
+++ sf_daq_broker/utils/load_module.py
@@ -1,9 +1,9 @@
-import pathlib
 import importlib.util as ilu
+from pathlib import Path
 
 
 def load_module(file_path, module_name=None):
-    module_name = module_name or pathlib.Path(file_path).stem
+    module_name = module_name or Path(file_path).stem
     spec = ilu.spec_from_file_location(module_name, file_path)
     module = ilu.module_from_spec(spec)
     spec.loader.exec_module(module)
